fa3c74a75fdf6f60f4b34d62d005bdd6a3468a38
tanaka <lemo.nade.engineer.room@gmail.com>
2023-05-03 20:26:18 +0900

    初期化テストを削除

--- api/Tests/EntityTests/User/UserTests.swift
+++ api/Tests/EntityTests/User/UserTests.swift
@@ -9,13 +9,6 @@ final class UserTests: XCTestCase {
         .init(id: .init(UUID()), name: "Cancy"),
     ]
     
-    func test初期化() {
-        let id = User.ID(UUID())
-        let user = User(id: id, name: "ユーザー名")
-        XCTAssertEqual(user.id, id)
-        XCTAssertEqual(user.name, "ユーザー名")
-    }
-    
     func test比較すると名前で比較される() {
         XCTAssertLessThan(Self.values[0], Self.values[1])
     }
