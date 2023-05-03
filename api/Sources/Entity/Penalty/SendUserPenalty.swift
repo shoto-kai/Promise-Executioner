f175029b9089b7067910ccb96f6023a0b76b8950
tanaka <lemo.nade.engineer.room@gmail.com>
2023-05-03 20:35:36 +0900

    メッセージ送信ペナルティを作成

---
+++ api/Sources/Entity/Penalty/SendUserPenalty.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+public struct SendUserPenalty: Hashable, Codable, Identifiable {
+    
+    public var id: ID
+    
+    /// 送り先ユーザー
+    public var destination: User
+    
+    /// ペナルティで送るものの概要
+    public var overview: String
+    
+    /// メモ
+    public var note: String
+    
+    /// 送信する内容
+    public var content: String
+    
+    /// 執行日時
+    public var executedAt: Date?
+    
+    public init(
+        id: ID,
+        destination: User,
+        overview: String,
+        note: String,
+        content: String,
+        executedAt: Date? = nil
+    ) {
+        self.id = id
+        self.destination = destination
+        self.overview = overview
+        self.note = note
+        self.content = content
+        self.executedAt = executedAt
+    }
+    
+}
+
+extension SendUserPenalty {
+    public struct ID: Hashable, Codable {
+        public let value: UUID
+        public init(_ value: UUID) {
+            self.value = value
+        }
+    }
+}
