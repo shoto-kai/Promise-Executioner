72ac3c12664974874cdde6cc7f8ccf4e866058c4
tanaka <lemo.nade.engineer.room@gmail.com>
2023-05-08 01:15:03 +0900

    NotificationCreatableを実装

---
+++ api/Sources/Persistence/Repository/Notification/AppNotificationRepository+NotificationCreatable.swift
@@ -0,0 +1,12 @@
+import Fluent
+import Entity
+import Repository
+
+extension AppNotificationRepository: NotificationCreatable {
+    public func create(
+        _ notification: Entity.AppNotification,
+        of userID: Entity.User.ID
+    ) async throws {
+        try await AppNotification(notification, of: userID).create(on: db)
+    }
+}
