b2b6ce93979e70e80bf119b97f8a81a2396a6682
tanaka <lemo.nade.engineer.room@gmail.com>
2023-05-16 23:59:06 +0900

    protocol修正

--- api/Sources/Repository/Notification/PairNotificationRepository.swift
+++ api/Sources/Repository/Notification/PairNotificationRepository.swift
@@ -1,9 +1,17 @@
+import Foundation
 import Entity
 
+/// ユーザーの全てのユーザー間通知を取得する
 public protocol AllPairNotificationTakeable {
     func takeAll(of: User.ID) async throws -> [PairNotification]
 }
 
+/// ユーザー間通知を作成する
 public protocol PairNotificationCreatable {
     func create(_ notification: PairNotification) async throws
+}
+
+/// ユーザー間通知のreadを更新する
+public protocol PairNotificationReadAtUpdatable {
+    func update(_ readAt: Date) async throws
 }
