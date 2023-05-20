3f69d7095b42b63a02f37779e95940ce1c27be25
tanaka <lemo.nade.engineer.room@gmail.com>
2023-05-20 14:13:58 +0900

    Taskモデルの修正

--- app/src/models/task/task.test.ts
+++ app/src/models/task/task.test.ts
@@ -1,82 +1,18 @@
-import { describe } from "vitest";
-import { Task } from "~/models/task/task";
-import { Restrictions } from "~/models/task/restriction/restrictions";
-import { DateLimitRestriction } from "~/models/task/restriction/dateLimitRestriction";
-import { Penalties } from "~/models/task/penalty/penalties";
-import { SendUserPenalty } from "~/models/task/penalty/sendUserPenalty";
-import { AnotherUser } from "~/models/user/anotherUser";
-import { User } from "~/models/user/user";
+import { describe, expect } from "vitest";
+import { AppTask, AppTaskState } from "~/models/task/task";
 
 describe("Task Tests", () => {
-  const task = new Task(
-    "4774DD61-245D-4D7B-B64A-A85107419AA4",
-    "BBQ集合時間",
-    "BBQ集合時間は18:00です",
-    new Date("2021-08-01T18:00:00+09:00"),
-    new Date("2021-08-01T17:55:00+09:00"),
-    undefined,
-    new Restrictions([
-      new DateLimitRestriction(
-        "D3B8116F-54FA-4568-8750-D30CFDC72342",
-        new Date("2021-08-01T18:00:00+09:00"),
-        new Date("2021-08-01T17:55:00+09:00")
-      ),
-    ]),
-    new Penalties([
-      new SendUserPenalty(
-        "DC4F1FEA-8C79-418D-B6BA-E5ADD633D037",
-        new AnotherUser(
-          new User("B45E919E-9CAD-4956-AF61-AFE9F32C47E2", "田中"),
-          false
-        ),
-        "PayPay 500円",
-        "",
-        "https://example.com"
-      ),
-    ])
-  );
-
-  const json = {
-    id: "4774DD61-245D-4D7B-B64A-A85107419AA4",
-    title: "BBQ集合時間",
-    note: "BBQ集合時間は18:00です",
-    limitDate: "2021-08-01T09:00:00Z",
-    completedAt: "2021-08-01T08:55:00Z",
-    failedAt: undefined,
-    restrictions: {
-      dateLimitRestrictions: [
-        {
-          id: "D3B8116F-54FA-4568-8750-D30CFDC72342",
-          limit: "2021-08-01T09:00:00Z",
-          completedAt: "2021-08-01T08:55:00Z",
-        },
-      ],
-    },
-    penalties: {
-      sendUserPenalties: [
-        {
-          id: "DC4F1FEA-8C79-418D-B6BA-E5ADD633D037",
-          destinationUser: {
-            user: {
-              id: "B45E919E-9CAD-4956-AF61-AFE9F32C47E2",
-              name: "田中",
-            },
-            isMyFriend: false,
-          },
-          overview: "PayPay 500円",
-          note: "",
-          content: "https://example.com",
-          executedAt: undefined,
-        },
-      ],
-    },
-  };
-
-  test("encode Test", () => {
-    expect(task.encode()).toEqual(json);
-  });
-
-  test("decode Test", () => {
-    expect(Task.decode(json)).toEqual(task);
+  test("初期値のテスト", () => {
+    const id = crypto.randomUUID();
+    const expected = new AppTask(
+      id,
+      "",
+      "",
+      0,
+      AppTaskState.incomplete,
+      undefined
+    );
+    const actual = AppTask.init(id);
+    expect(actual).toEqual(expected);
   });
 });
