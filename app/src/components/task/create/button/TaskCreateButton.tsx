81c1e167a9564b6202477271125f8108e840cc06
yusukezemi <106569487+nakami3316@users.noreply.github.com>
2023-05-20 13:02:35 +0900

    スクロール時にボタンが動くので修正

--- app/src/components/task/create/button/TaskCreateButton.tsx
+++ app/src/components/task/create/button/TaskCreateButton.tsx
@@ -1,14 +1,17 @@
 import { IonButton } from "@ionic/react";
 
+import "./TaskCreateButton.css";
+
 export default function TaskCreateButton({ onClick }: { onClick: () => void }) {
   return (
     <IonButton
+      slot="fixed"
       shape="round"
       color="primary"
       onClick={onClick}
       class="ion-margin"
     >
-      タスク作成ボタン
+      作成
     </IonButton>
   );
 }
