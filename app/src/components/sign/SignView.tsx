9d6088d711a76983d4f28719feebe0905cc961fe
YuugouOhno <yuugou.ohno@gmail.com>
2023-05-20 17:18:33 +0900

    #15 signページの追加

--- app/src/components/sign/SignView.tsx
+++ app/src/components/sign/SignView.tsx
@@ -1,4 +1,14 @@
+import "./SignView.css";
 import { useState, useMemo } from "react";
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonIcon
+} from "@ionic/react";
+import { logoGithub, logoGoogle } from 'ionicons/icons';
 import { SignViewState as State } from "~/components/sign/SignView.state";
 import { SignViewProps as Props } from "~/components/sign/SignView.props";
 import { SignViewModel as Model } from "~/components/sign/SignView.model";
@@ -7,9 +17,25 @@ export default function SignView({ props }: { props: Props }) {
   const [state, setState] = useState(State.init);
   const m = useMemo(() => new Model(props, state, setState), [props, state]);
   return (
-    <>
-      <button onClick={m.logInByGoogle}>Googleでログイン</button>
-      <button onClick={m.logInByGitHub}>GitHubでログイン</button>
-    </>
+    <IonCard class="ion-align-items-center">
+      <IonCard>
+        <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+        <IonCardHeader>
+          <IonCardTitle>アカウント登録/ログイン</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent class="sign-button">
+          <IonButton color="dark" fill="outline" expand="block" onClick={m.logInByGitHub}>
+            <IonIcon slot="start" icon={logoGoogle}></IonIcon>
+            Googleでログイン
+          </IonButton>
+        </IonCardContent>
+        <IonCardContent class="sign-button">
+          <IonButton class="sign-button" color="dark" fill="outline" expand="block" onClick={m.logInByGoogle}>
+            <IonIcon slot="start" icon={logoGithub}></IonIcon>
+            GitHubでログイン
+          </IonButton>
+        </IonCardContent>
+      </IonCard>
+    </IonCard>
   );
 }
--- app/src/pages/SignPage.tsx
+++ app/src/pages/SignPage.tsx
@@ -1,9 +1,6 @@
 import {
   IonContent,
-  IonHeader,
   IonPage,
-  IonTitle,
-  IonToolbar,
 } from "@ionic/react";
 import React, { FC } from "react";
 import SignView from "~/components/sign/SignView";
@@ -11,17 +8,7 @@ import SignView from "~/components/sign/SignView";
 const SignPage: FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Sign</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Sign</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <SignView props={{}} />
       </IonContent>
     </IonPage>
